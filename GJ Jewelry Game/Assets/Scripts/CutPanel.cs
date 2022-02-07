4af9a2ed8670d56172afade32d43081500a2660f
raramai-coder <1846409@students.wits.ac.za>
2022-02-07 17:56:10 +0200

    cutting of gem (prelim) and opening of final panel

--- GJ Jewelry Game/Assets/Scripts/CutPanel.cs
+++ GJ Jewelry Game/Assets/Scripts/CutPanel.cs
@@ -5,22 +5,31 @@ using UnityEngine.UI;
 
 public class CutPanel : MonoBehaviour
 {
-    [SerializeField] private GameObject cutOptionsPanel;
+    [SerializeField] private GameObject cutOptionsPanel, hammerPanel, finalPanel;
 
-    [SerializeField] private Image whiteGemImg, purpleGemImg, pinkGemImg;
+    [SerializeField] private Image whiteGemImg, purpleGemImg, pinkGemImg, finalGemImg;
     [SerializeField] private Image whiteGemTick, purpleGemTick, pinkGemTick, cut1Tick, cut2Tick, cut3Tick;
 
-    private bool selectedGem;
+    private bool selectedGem, selectedCut;
+
+    private int hits = 0;
+    private int cut = 0;
+    private string finalGem = "none";
 
     // Start is called before the first frame update
     void Start()
     {
         cutOptionsPanel.SetActive(false);
+        hammerPanel.SetActive(false);
+        finalPanel.SetActive(false);
+
         selectedGem = false;
+        selectedCut = false;
 
         whiteGemImg.enabled = false;
         purpleGemImg.enabled = false;
         pinkGemImg.enabled = false;
+        finalGemImg.enabled = false;
 
         pinkGemTick.enabled = false;
         purpleGemTick.enabled = false;
@@ -42,11 +51,23 @@ public class CutPanel : MonoBehaviour
         {
             cutOptionsPanel.SetActive(false);
         }
+
+        if (selectedCut)
+        {
+            hammerPanel.SetActive(true);
+        }
+        else
+        {
+            hammerPanel.SetActive(false);
+        }
     }
 
     #region gems
     public void WhiteGem()
     {
+        /*selectedGemImg.enabled = true;
+        selectedGemImg = whiteGemImg;*/
+
         whiteGemImg.enabled = true;
         purpleGemImg.enabled = false;
         pinkGemImg.enabled = false;
@@ -56,6 +77,8 @@ public class CutPanel : MonoBehaviour
         whiteGemTick.enabled = true;
 
         selectedGem = true;
+        //gem = 1;
+        finalGem = "white";
     } 
     public void PurpleGem()
     {
@@ -68,6 +91,8 @@ public class CutPanel : MonoBehaviour
         whiteGemTick.enabled = false;
 
         selectedGem = true;
+        //gem = 2;
+        finalGem = "purple";
     }
     public void PinkGem()
     {
@@ -80,21 +105,53 @@ public class CutPanel : MonoBehaviour
         whiteGemTick.enabled = false;
 
         selectedGem = true;
+        //gem = 3;
+        finalGem = "pink";
     }
     #endregion
 
+    
     public void Cut1()
     {
         cut1Tick.enabled = true;
+        selectedCut = true;
+        cut = 1;
     }
 
     public void Cut2()
     {
         cut2Tick.enabled = true;
+        selectedCut = true;
+        cut = 2;
     }
 
     public void Cut3()
     {
         cut3Tick.enabled = true;
+        selectedCut = true;
+        cut = 3;
+    }
+
+    public void Hammer()
+    {
+        if(hits == 4)
+        {
+            whiteGemImg.enabled = false;
+            purpleGemImg.enabled = false;
+            pinkGemImg.enabled = false;
+
+            finalGemImg.enabled = true;
+
+            
+            //Debug.Log("enough smashing");
+            
+            //make hammer diappear
+        }else if (hits == 5)
+        {
+            finalPanel.SetActive(true);
+        }
+        
+        ++hits;
+        Debug.Log(hits);
     }
 }
